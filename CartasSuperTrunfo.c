24062446e56ae4e812a70dd228e88a7ec98bf909
stegebert <105332570+stegebert@users.noreply.github.com>
2025-04-22 22:22:23 +0000

    CadastrandoCartas.1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,7 +9,16 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+
+    // ===== CARTA 1 - Brasil =====
+    char codigo1[] = "A01"; // Código da cidade (A = RS)
+    char nome1[] = "Porto Alegre";
+    char estado1[] = "Rio Grande do Sul";
+    char pais1[] = "Brasil";
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
